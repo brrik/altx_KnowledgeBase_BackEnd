0139b419b4dfaa5a48bbaf3f07891b64d463d86c
brrik0677 <leo.luvs.footwork@gmail.com>
2025-06-15 16:53:00 +0900

    大西：初期設定関係

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware #CORS用
+import gspread
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+
+#ここからGspread用=============================================
+
+#GspreadからGoogleに接続する用のデータを環境変数（OS）からとってくる
+Auth = "./norse-lotus-423606-i2-353a26d9cd49.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = Auth
+
+#Gspreadから、Googleの接続用アカウントを使えるようにする認証作業
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(Auth, scope)
+Client = gspread.authorize(credentials)
+
+#スプシに接続する
+SpreadSheet = Client.open_by_key("まだスプシ作ってない")
+knowledge_sheet = SpreadSheet.worksheet("決めた後スプシのシート名書く") #ナレッジ用シート
+comment_sheet = SpreadSheet.worksheet("決めた後スプシのシート名書く") #コメント用シート
+
+async def getTest():
+    print("hello world")
+
+
+
+
+
+
+#ここからFastAPI用=============================================
+# CORS
+# 消さないで　担当大西
+
+app = FastAPI() #FastAPIインスタンス作成
+
+# 外部からの通信を受け付けるためのやつ
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+# ここまで
+
+@app.get("/")
+async def getMain():
+    print("hello, world")
