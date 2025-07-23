711c56c6c924a60bf1b72744c79779ff13aceaf7
masuso <sota0504@outlook.jp>
2025-07-23 15:38:40 +0900

    升村タグ追加

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import gspread
 import os
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
+from pydantic import BaseModel
+import pandas as pd
+
 
 #ここからGspread用=============================================
 
@@ -21,17 +24,16 @@ SpreadSheet = Client.open_by_key("1vwzM38sPQCTwS2dSlq9HQuutd823psOz-KV2wd0HZyE")
 knowledge_sheet = SpreadSheet.worksheet("ナレッジ") #ナレッジ用シート
 comment_sheet = SpreadSheet.worksheet("コメント") #コメント用シート
 
-async def getTest():
-    print("hello world")
 
 
-async def test_Tomiyasu():
-    print("I am human.")
-
-#いまいアップデート
-print ("hello")
-
-
+def get_all_value_rensyu():
+    values = knowledge_sheet.get_all_values()
+    header = values[0]
+    body = values[1:]
+    df = pd.DataFrame(body, columns=header)
+    selected_df = df[["ID", "Title", "PostedBy"]]
+    print(selected_df.to_string(index=False))
+get_all_value_rensyu()
 
 
 #ここからFastAPI用=============================================
@@ -49,6 +51,8 @@ app.add_middleware(
     allow_headers=["*"]
 )
 # ここまで
+
+#### 以下get通信 ####
 
 @app.get("/")
 async def getMain():
@@ -76,3 +80,117 @@ async def test_def():
 @app.get("/gorenkin-saiko")
 async def gorenkin():
     return "これはmasuのテストです"
+
+#いまいテスト
+@app.get("/natsubategimi")
+async def natsubate():
+    return "これはimaiのテストです"
+
+#### 以上get通信 ####
+
+#### 以下post通信 ####
+
+# 受け取るデータの型を定義
+class Item(BaseModel):
+    title: str
+    name: str
+    detail: str
+    tag1: str
+    tag2: str   #今井追加
+    tag3: str   #升村追加
+
+# POSTリクエストを受け取るエンドポイント
+@app.post("/post-test")
+async def post_test(item: Item):
+    return {"message": "受信成功", "受け取ったデータ": item}
+
+
+
+#### 以上post通信 ####
+ 
+
+
+#スプレッドシートへの投稿用コード_川空作成分
+
+#関数定義
+
+#ナレッジ投稿関数
+def add_knowledge(knowledge_sheet, data):
+
+#ヘッダー情報取得
+    knowledge_header = knowledge_sheet.row_values(1)
+
+#IDの採番
+    all_rows = knowledge_sheet.get_all_values()
+    id_index = knowledge_header.index("ID")
+    existing_ids = [int(row[id_index]) for row in all_rows[1:] if row[id_index].isdigit()]
+    new_id = max(existing_ids, default=0) + 1
+
+#行データの作成
+    new_row = [""] * len(knowledge_header)
+    new_row[id_index] = str(new_id)
+
+    for key,value in data.items():
+        if key in knowledge_header:
+            col_index = knowledge_header.index(key)
+            new_row[col_index] = value
+
+#シートへ書込み
+    next_row = len(all_rows) + 1 #次の空行を取得（既存行数+1）
+    knowledge_sheet.insert_row(new_row,next_row)
+    print(f"書き込み完了：ID {new_id}")
+
+
+
+#コメント投稿関数
+def add_comment(comment_sheet, comment_data):
+#ヘッダー情報取得
+    comment_header = comment_sheet.row_values(1)
+
+#IDの採番
+    all_rows = comment_sheet.get_all_values()
+    id_index =comment_header.index("CommentID") 
+    existing_ids = [int(row[id_index]) for row in all_rows[1:] if row[id_index].isdigit()]
+    new_comment_id = max(existing_ids, default=0) + 1
+
+#行データの作成
+    new_row = [""] * len(comment_header)
+    new_row[id_index] = str(new_comment_id)
+
+    for key,value in comment_data.items():
+        if key in comment_header:
+            col_index = comment_header.index(key)
+            new_row[col_index] = value
+
+#シートへ書込み
+    next_row = len(all_rows) +1
+    comment_sheet.insert_row(new_row,next_row)
+    print(f"コメントの書き込み完了：ID {new_comment_id}")
+
+
+
+#投稿データと実行
+
+#ナレッジ投稿データ（Jsonができていないので仮）
+data = {
+    "Title": "最近のブーム",
+    "PostedBy": "川空のどか",
+    "Content": "蒸籠でごはんをつくること！",
+    "Tag1": "ご飯",
+    "Tag2": "日記",
+    "Tag3": "生活"
+}
+
+#ナレッジ関数実行
+add_knowledge(knowledge_sheet, data)
+
+
+#コメント投稿データ（Jsonができていないので仮）
+comment_data = {
+    "KnowledgeID": "3",  #対象のIDナレッジにコメントする
+    "PostedBy": "川空のどか",
+    "Content": "めっちゃ参考になりました！最高！！！"
+}
+
+#コメント関数実行
+add_comment(comment_sheet, comment_data)
